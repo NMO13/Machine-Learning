c73969ee346fa23203cfa9ec680089a646ffd7e7
Martin Ennemoser <martin.ennemoser@gmx.at>
2018-08-06 00:00:11 +0200

    added neural network alorithm

---
+++ neural network/my_neural_network.py
@@ -0,0 +1,147 @@
+# https://mattmazur.com/2015/03/17/a-step-by-step-backpropagation-example/
+import numpy as np
+import mnist_loader
+
+class MyNeuralNet:
+    def __init__(self):
+        self.weights = list()
+        self.last_layer_neuron_count = 0
+
+    @property
+    def input(self):
+        return self._input
+
+    @input.setter
+    def input(self, value):
+        self._input = value
+        self.last_layer_neuron_count = value.shape[1]
+
+    def add_layer(self, neuron_count, seed = None):
+        np.random.seed(seed)
+        weight_layer = np.random.rand(neuron_count, self.last_layer_neuron_count + 1)
+        # add a new layer of neurons and initialize weights randomly
+        self.weights.append(weight_layer)
+        self.last_layer_neuron_count = neuron_count
+
+    def learn(self, epochs, eta, test_data = None):
+        if self.last_layer_neuron_count == 0:
+            raise ValueError('No output layer specified.')
+        for i in range(epochs):
+            # apply gradient descent on cost function and
+            # update all weights
+            self.feed_forward()
+            self.update(self.backward(), eta)
+            if test_data:
+                print('Epoch {0}: {1} / {2}'.format(i, self.evaluate(test_data), len(test_data)))
+            else:
+                print('Epoch {0} complete'.format(i));
+
+
+    def classify(self, input):
+        if len(self.weights) == 0:
+            raise ValueError('Network is not initialized.')
+        a = input
+        for w in self.weights:
+            #a1 = np.zeros((input.shape[0], input.shape[1]+1))
+            #a1[:, :-1] = a
+            a = np.append(a, [[1] for x in a], axis=1)
+            z = np.dot(a, w.T)
+            a = self.sigmoid(z)
+        return a
+
+    def feed_forward(self):
+        a = np.array(self._input, copy=True)
+        self.a_matrix = [a]
+        for w in self.weights:
+            # add bias term
+            a = np.append(a, [[1] for x in a], axis=1)
+            z = np.dot(a, w.T)
+            a = self.sigmoid(z)
+            self.a_matrix.append(a)
+        self.output = a
+        #todo
+        #print(self.calc_loss())
+
+    def backward(self):
+        w_b_gradient = [np.zeros(w.shape) for w in self.weights]
+        if self.a_matrix[-1].shape != self.y.shape:
+            raise ValueError('The dimensions of the y and output layer do not match')
+        ############# output layer
+        # 1. how much did we miss in the output neuron?
+        error = self.a_matrix[-1] - self.y
+
+        # 2. how much does sigma change?
+        z_prime = self.a_matrix[-1] * (1 - self.a_matrix[-1])
+
+        # 3. calculate delta term for output layer L
+        delta_L = (error * z_prime)
+
+        # 4. multiply each output with delta term
+        # and save new weights in matrix
+        w_b_gradient[-1][:, :-1] = np.dot(delta_L.T, self.a_matrix[-2])
+
+        # save new biases as well
+        w_b_gradient[-1][:, -1] = np.sum(delta_L.T, axis=1)
+
+
+        last_delta = delta_L
+        ########### hidden layers
+        for n in range(len(self.weights) - 2, -1, -1):
+            weights_prev_layer = self.weights[n + 1][:, :-1]
+            delta_l2 = np.dot(last_delta, weights_prev_layer) * (self.a_matrix[n + 1] * (1 - self.a_matrix[n + 1]))
+            w_b_gradient[n][:, :-1] = np.dot(delta_l2.T, self.a_matrix[n])
+            w_b_gradient[n][:, -1] = np.sum(delta_l2.T, axis=1)
+
+        return w_b_gradient
+
+
+    def calc_loss(self):
+        res = (self.y - self.output.flatten()) ** 2
+        return sum(res) / 2 / len(self.y)
+
+    def sigmoid(self, z):
+        # see https://stackoverflow.com/questions/23128401/overflow-error-in-neural-networks-implementation
+        #z = np.clip(z, -500, 500)
+        return 1 / (1 + np.exp(-z))
+
+    def update(self, w_b_gradient, eta):
+        for i in range(len(self.weights)):
+            tmp = self.weights[i] - eta * w_b_gradient[i]
+            self.weights[i] = tmp
+
+    def setup_test_conf(self):
+        self.weights.append(np.array([[0.15, 0.2, 0.35], [0.25, 0.3, 0.35]]))
+        self.weights.append(np.array([[0.4, 0.45, 0.6], [0.5, 0.55, 0.6]]))
+
+    def evaluate(self, test_data):
+        """Return the number of test inputs for which the neural
+        network outputs the correct result. Note that the neural
+        network's output is assumed to be the index of whichever
+        neuron in the final layer has the highest activation."""
+        test_results = [(np.argmax(self.classify(np.array([x.flatten()]))), y)
+                        for (x, y) in test_data]
+        return sum(int(x == y) for (x, y) in test_results)
+
+if __name__ == '__main__':
+    training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+    training_data = np.array(training_data[:1000])
+    X = training_data[:, 0]
+    X = np.array(list(map(lambda x: x.flatten(), X)))
+
+    y = training_data[:, 1]
+    y = np.array(list(map(lambda x: x.flatten(), y)))
+    #X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+    #y = np.array([0, 1, 1, 1])
+
+    #X = np.array([[0.05, 0.10]])
+    #y = np.array([[0.01, 0.99]])
+
+    nn = MyNeuralNet()
+    nn.input = X
+    nn.y = y
+    nn.add_layer(1)
+    nn.add_layer(10)
+    #nn.add_layer(2)
+    nn.learn(1500, 0.1, test_data)
+    #print(nn.output)
+    print(nn.classify(np.array([X[0]])))
---
+++ neural network/neural_net_test.py
@@ -0,0 +1,182 @@
+import unittest
+import numpy as np
+from under_work import MyNeuralNet
+
+class TestNeuralNet(unittest.TestCase):
+
+    # used for gradient checking
+    # see https://www.youtube.com/watch?v=P6EtCVrvYPU&list=PLLssT5z_DsK-h9vYZkQkYNWcItqhlRJLN&index=54
+    def calc_gradients(self, nn):
+        epsilon = 0.00001
+        def cost():
+            c = 0.5 * (nn.output - nn.y) ** 2
+            return sum(c.flatten())
+
+        grad_approx = [[[0 for k in range(len(nn.weights[i][j]))] for j in range(len(nn.weights[i]))] for i in range(len(nn.weights))]
+        for layer in range(len(nn.weights)):
+            for w0 in range(len(nn.weights[layer])):
+                for w1 in range(len(nn.weights[layer][w0])):
+                    value = nn.weights[layer][w0][w1]
+                    nn.weights[layer][w0][w1] = value + epsilon
+                    nn.feed_forward()
+                    c0 = cost()
+                    nn.weights[layer][w0][w1] = value - epsilon
+                    nn.feed_forward()
+                    c1 = cost()
+                    res = c0 - c1
+                    grad_approx[layer][w0][w1] = res / (2*epsilon)
+                    nn.weights[layer][w0][w1] = value
+        return grad_approx
+
+    def test_raise_no_layers(self):
+        nn = MyNeuralNet()
+        with self.assertRaises(ValueError):
+            nn.learn(1, 0.3)
+            nn.last_layer_neuron_count
+
+
+    def test_raise_illegal_output(self):
+        X = np.array([[0, 0, 1],
+                      [0, 1, 1],
+                      [1, 0, 1],
+                      [1, 1, 1],
+                      [0, 0, 0]])
+        y = np.array([[0, 0], [1, 1], [1, 1], [0, 0], [0, 0]])
+        nn = MyNeuralNet()
+        nn.input = X
+        nn.y = y
+
+        nn.add_layer(1, 0)
+        with self.assertRaises(ValueError):
+            nn.learn(1, 0.5)
+
+    def test_simple_net(self):
+        X = np.array([[0.05]])
+        y = np.array([[0.01]])
+
+        nn = MyNeuralNet()
+        nn.input = X
+        nn.y = y
+        nn.add_layer(1, 0)
+        nn.learn(1500, 0.3)
+        self.assertEqual(len(nn.weights), 1)
+        self.assertEqual(nn.weights[0].shape, (1, 2))
+        self.assertEqual(nn.weights[0][0][0], 0.35288406943063816)
+
+    def test_backprop_1_iteration(self):
+        X = np.array([[0.05, 0.10]])
+        y = np.array([[0.01, 0.99]])
+
+        nn = MyNeuralNet()
+        nn.input = X
+        nn.y = y
+        nn.setup_test_conf()
+        nn.learn(1, 0.5)
+        self.assertEqual(nn.weights[0][0][0], 0.14978071613276281)
+        self.assertEqual(nn.weights[0][0][1], 0.19956143226552567)
+        self.assertEqual(nn.weights[0][1][0], 0.24975114363236958)
+        self.assertEqual(nn.weights[0][1][1], 0.29950228726473915)
+
+        self.assertEqual(nn.weights[1][0][0], 0.35891647971788465)
+        self.assertEqual(nn.weights[1][0][1], 0.4086661860762334)
+        self.assertEqual(nn.weights[1][1][0], 0.5113012702387375)
+        self.assertEqual(nn.weights[1][1][1], 0.5613701211079891)
+
+    def test_backprop_1500_iteration(self):
+        X = np.array([[0.05, 0.10]])
+        y = np.array([[0.01, 0.99]])
+
+        nn = MyNeuralNet()
+        nn.input = X
+        nn.y = y
+        nn.setup_test_conf()
+        nn.learn(1500, 0.5)
+        self.assertEqual(nn.weights[0][0][0], 0.1747576240731027)
+        self.assertEqual(nn.weights[0][0][1], 0.24951524814620638)
+        self.assertEqual(nn.weights[0][1][0], 0.2740253089080296)
+        self.assertEqual(nn.weights[0][1][1], 0.3480506178160598)
+        self.assertEqual(nn.output.shape, (1,2))
+
+    def test_multiple_input_1_neuron_1_layer(self):
+        X = np.array([[0, 0, 1],
+                      [0, 1, 1],
+                      [1, 0, 1],
+                      [1, 1, 1],
+                      [0, 0, 0]])
+        y = np.array([[0], [1], [1], [0], [0]])
+        nn = MyNeuralNet()
+        nn.input = X
+        nn.y = y
+
+        nn.add_layer(4, 0)
+        nn.add_layer(1, 0)
+        nn.learn(1500, 1)
+        self.assertEqual(list(map(lambda x: int(round(x[0])), nn.output)), [0, 1, 1, 0, 0])
+
+    def test_multiple_input_2_neuron_1_layer(self):
+        X = np.array([[0, 0, 1],
+                      [0, 1, 1],
+                      [1, 0, 1],
+                      [1, 1, 1],
+                      [0, 0, 0]])
+        y = np.array([[0, 0], [1, 1], [1, 1], [0, 0], [0, 0]])
+        nn = MyNeuralNet()
+        nn.input = X
+        nn.y = y
+        nn.add_layer(4, 0)
+        nn.add_layer(2, 0)
+        nn.learn(1500, 1)
+        self.assertEqual(list(map(lambda x: [int(round(x[0])), int(round(x[1]))], nn.output)), [[0, 0], [1, 1], [1, 1], [0, 0], [0, 0]])
+
+
+    def test_multiple_input_50_neuron_2_layer(self):
+        X = np.array([[0, 0],
+                      [0, 1],
+                      [1, 0],
+                      [1, 1],
+                      [0, 0]])
+        y = np.array([[0], [1], [1], [0], [0]])
+        nn = MyNeuralNet()
+        nn.input = X
+        nn.y = y
+
+        nn.add_layer(4, 0)
+        nn.add_layer(1, 0)
+        nn.learn(1500, 0.5)
+        self.assertEqual(list(map(lambda x: int(round(x[0])), nn.output)), [0, 1, 1, 0, 0])
+
+    def test_gradient_checking(self):
+        X = np.array([[0.05, 0.10]])
+        y = np.array([[0.01, 0.99]])
+
+        nn = MyNeuralNet()
+        nn.input = X
+        nn.y = y
+        nn.setup_test_conf()
+        grad_approx = np.array(self.calc_gradients(nn))
+        nn.feed_forward()
+        grad = np.array(nn.backward())
+        self.assertTrue(np.allclose(grad, grad_approx))
+
+    def test_classify(self):
+        X = np.array([[0, 0],
+                      [0, 1],
+                      [1, 0],
+                      [1, 1],
+                      [0, 0]])
+        y = np.array([[0], [1], [1], [0], [0]])
+        nn = MyNeuralNet()
+        nn.input = X
+        nn.y = y
+
+        nn.add_layer(4, 0)
+        nn.add_layer(1, 0)
+        nn.learn(1500, 10)
+        res = nn.classify(np.array([[0, 0], [1, 0]]))
+        self.assertEqual(int(round(res[0][0])), 0)
+        self.assertEqual(int(round(res[1][0])), 1)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
