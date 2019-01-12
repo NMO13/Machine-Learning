5d3ee251913b422833b553d72dd8255550046683
NMO13 <martin.ennemoser@gmx.at>
2019-01-12 02:37:03 +0100

    Added more negative states

--- reinforcement learning/mouse_world.py
+++ reinforcement learning/mouse_world.py
@@ -29,8 +29,18 @@ class MouseWorld:
                     print("%.2f|" % v, end="")  # -ve sign takes up an extra space
             print("")
 
+    def print_best_policy(self):
+        for i in range(self.width):
+            print("---------------------------")
+            for j in range(self.height):
+                stats = self.Q[(i, j)]
+                action = max(stats.keys(), key=(lambda key: stats[key]))
+                print("  %s  |" % action, end="")
+            print("")
+
     def game_over(self):
-        return self.current_state == (1, 1) or self.current_state == (1, 2)
+        return self.current_state == (1, 1) or self.current_state == (1, 2)\
+               or self.current_state == (3, 4) or self.current_state == (0, 2)
 
     def move(self):
         eps = 0.3
@@ -67,10 +77,22 @@ class MouseWorld:
 
 
 if __name__ == "__main__":
-    mw = MouseWorld(2, 3, {(0, 1): 0, (1, 0): 0, (1, 2): 1, (1, 1): -1})
+    import time
+    start = time.time()
+    mw = MouseWorld(5, 5, {(0, 1): 0, (1, 0): 0, (4, 4): 1, (1, 1): -1, (3, 4): -1, (0, 2): -1})
     mw.print_values(mw.rewards)
-    for i in range(10000):
+    # Exploration parameters
+    epsilon = 1.0  # Exploration rate
+    max_epsilon = 1.0  # Exploration probability at start
+    min_epsilon = 0.01  # Minimum exploration probability
+    decay_rate = 0.005  # Exponential decay rate for exploration prob
+    for episode in range(10000):
         mw.current_state = (0, 0)
         while not mw.game_over():
             mw.move()
+        # Reduce epsilon (because we need less and less exploration)
+        epsilon = min_epsilon + (max_epsilon - min_epsilon) * np.exp(-decay_rate * episode)
+    end = time.time()
+    mw.print_best_policy()
     print('Finished')
+    print('time: {}'.format(end - start))
