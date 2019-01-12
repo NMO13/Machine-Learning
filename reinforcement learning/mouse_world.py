c54bc52e94abdb5a785240b14972a4c1221ca3ea
NMO13 <martin.ennemoser@gmx.at>
2019-01-12 13:47:56 +0100

    exchanged width/height

--- reinforcement learning/mouse_world.py
+++ reinforcement learning/mouse_world.py
@@ -11,17 +11,17 @@ class MouseWorld:
 
         self.rewards = {}
         self.Q = {}
-        for i in range(width):
-            for j in range(height):
+        for i in range(self.height):
+            for j in range(self.width):
                 self.rewards[(i, j)] = local_rewards.get((i, j), 0)
                 self.Q[(i, j)] = {}
                 for action in self.actions:
                     self.Q[(i, j)][action] = 0
 
     def print_values(self, values):
-        for i in range(self.width):
+        for i in range(self.height):
             print("---------------------------")
-            for j in range(self.height):
+            for j in range(self.width):
                 v = values.get((i, j), 0)
                 if v >= 0:
                     print(" %.2f|" % v, end="")
@@ -30,9 +30,9 @@ class MouseWorld:
             print("")
 
     def print_best_policy(self):
-        for i in range(self.width):
+        for i in range(self.height):
             print("---------------------------")
-            for j in range(self.height):
+            for j in range(self.width):
                 stats = self.Q[(i, j)]
                 action = max(stats.keys(), key=(lambda key: stats[key]))
                 print("  %s  |" % action, end="")
@@ -42,8 +42,7 @@ class MouseWorld:
         return self.current_state == (1, 1) or self.current_state == (1, 2)\
                or self.current_state == (3, 4) or self.current_state == (0, 2)
 
-    def move(self):
-        eps = 0.3
+    def move(self, eps):
         number = np.random.uniform()
         action = None
         if number <= eps:
@@ -82,16 +81,16 @@ if __name__ == "__main__":
     mw = MouseWorld(5, 5, {(0, 1): 0, (1, 0): 0, (4, 4): 1, (1, 1): -1, (3, 4): -1, (0, 2): -1})
     mw.print_values(mw.rewards)
     # Exploration parameters
-    epsilon = 1.0  # Exploration rate
+    epsilon = 0.3  # Exploration rate
     max_epsilon = 1.0  # Exploration probability at start
     min_epsilon = 0.01  # Minimum exploration probability
     decay_rate = 0.005  # Exponential decay rate for exploration prob
     for episode in range(10000):
         mw.current_state = (0, 0)
         while not mw.game_over():
-            mw.move()
+            mw.move(epsilon)
         # Reduce epsilon (because we need less and less exploration)
-        epsilon = min_epsilon + (max_epsilon - min_epsilon) * np.exp(-decay_rate * episode)
+        #epsilon = 1/(episode+1)#min_epsilon + (max_epsilon - min_epsilon) * np.exp(-decay_rate * episode)
     end = time.time()
     mw.print_best_policy()
     print('Finished')
