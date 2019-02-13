57a6d1c65f5ae3b8bb2d7f686633a723c58e5615
NMO13 <martin.ennemoser@gmx.at>
2019-02-14 00:49:33 +0100

    Added neural network facility

---
+++ tensorflow/neural_net.py
@@ -0,0 +1,89 @@
+import tensorflow as tf
+import numpy as np
+training_epochs = 1500
+n_neurons_in_h1 = 60
+n_neurons_in_h2 = 60
+learning_rate = 0.01
+
+n_features = 784
+n_classes = 10
+
+tr_features = np.array([[0, 0, 1],
+              [0, 1, 1],
+              [1, 0, 1],
+              [1, 1, 1],
+              [0, 0, 0]])
+tr_labels = np.array([[0], [1], [1], [0], [0]])
+
+ts_features = np.array([[0, 0, 1], [1, 0, 1]])
+ts_labels = np.array([[0], [1]])
+
+import mnist_loader
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+training_data = np.array(training_data)
+tr_features = np.array(list(map(lambda x: x.flatten(), training_data[:, 0])))
+tr_labels = np.array(list(map(lambda x: x.flatten(), training_data[:, 1])))
+
+test_data = np.array(training_data)
+ts_features = np.array(list(map(lambda x: x.flatten(), test_data[:, 0])))
+ts_labels = np.array(list(map(lambda x: x.flatten(), test_data[:, 1])))
+
+# placeholdr tensors built to store features(in X) , labels(in Y) and dropout probability(in keep_prob)
+X = tf.placeholder(tf.float32, [None, n_features], name='features')
+Y = tf.placeholder(tf.float32, [None, n_classes], name='labels')
+keep_prob=tf.placeholder(tf.float32,name='drop_prob')
+
+W1 = tf.Variable(tf.truncated_normal([n_features, n_neurons_in_h1], mean=0, stddev=1 / np.sqrt(n_features)), name='weights1')
+b1 = tf.Variable(tf.truncated_normal([n_neurons_in_h1],mean=0, stddev=1 / np.sqrt(n_features)), name='biases1')
+y1 = tf.nn.tanh((tf.matmul(X, W1)+b1), name='activationLayer1')
+
+#network parameters(weights and biases) are set and initialized(Layer2)
+W2 = tf.Variable(tf.random_normal([n_neurons_in_h1, n_neurons_in_h2],mean=0,stddev=1/np.sqrt(n_features)),name='weights2')
+b2 = tf.Variable(tf.random_normal([n_neurons_in_h2],mean=0,stddev=1/np.sqrt(n_features)),name='biases2')
+#activation function(sigmoid)
+y2 = tf.nn.sigmoid((tf.matmul(y1,W2)+b2),name='activationLayer2')
+
+#output layer weights and biasies
+Wo = tf.Variable(tf.random_normal([n_neurons_in_h2, n_classes], mean=0, stddev=1/np.sqrt(n_features)), name='weightsOut')
+bo = tf.Variable(tf.random_normal([n_classes], mean=0, stddev=1/np.sqrt(n_features)), name='biasesOut')
+#activation function(softmax)
+a = tf.nn.sigmoid((tf.matmul(y2, Wo) + bo), name='activationOutputLayer')
+
+#cost function
+#cross_entropy = tf.reduce_mean(-tf.reduce_sum(Y * tf.log(a),reduction_indices=[1]))
+l = tf.losses.mean_squared_error(Y, a)
+#optimizer
+train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(l)
+
+#compare predicted value from network with the expected value/target
+correct_prediction = tf.equal(tf.arg_max(a, 1), tf.arg_max(Y, 1))
+#accuracy determination
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32), name="Accuracy")
+
+# initialization of all variables
+initial = tf.global_variables_initializer()
+
+# creating a session
+with tf.Session() as sess:
+    sess.run(initial)
+    # training loop over the number of epoches
+    batchsize=50
+    for epoch in range(training_epochs):
+        for i in range(len(tr_features)):
+            start = i
+            end = i + batchsize
+            x_batch = tr_features[start:end]
+            y_batch = tr_labels[start:end]
+            # feeding training data/examples
+            sess.run(train_step, feed_dict={X: x_batch, Y: y_batch, keep_prob: 0.5})
+
+            i += batchsize
+        # feeding testing data to determine model accuracy
+        y_pred = sess.run(a, feed_dict={X: ts_features, keep_prob: 1.0})
+        y_true = sess.run(tf.constant(ts_labels))
+        acc = sess.run(accuracy, feed_dict={X: ts_features, Y: ts_labels, keep_prob: 1.0})
+        # write results to summary file
+        # print accuracy for each epoch
+        print('epoch', epoch, acc)
+        print('---------------')
+        print(y_pred, y_true)
