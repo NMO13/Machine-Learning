dd43f26cfc9d2596d1c145ece01b6922b43f4f16
Martin Ennemoser <m.ennemoser@salesbeat.io>
2019-02-14 16:04:59 +0100

    Get network running

--- tensorflow/neural_net.py
+++ tensorflow/neural_net.py
@@ -21,8 +21,6 @@ ts_labels = np.array([[0], [1]])
 import mnist_loader
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 training_data = np.array(training_data)
-tr_features = np.array(list(map(lambda x: x.flatten(), training_data[:, 0])))
-tr_labels = np.array(list(map(lambda x: x.flatten(), training_data[:, 1])))
 
 test_data = np.array(training_data)
 ts_features = np.array(list(map(lambda x: x.flatten(), test_data[:, 0])))
@@ -35,7 +33,7 @@ keep_prob=tf.placeholder(tf.float32,name='drop_prob')
 
 W1 = tf.Variable(tf.truncated_normal([n_features, n_neurons_in_h1], mean=0, stddev=1 / np.sqrt(n_features)), name='weights1')
 b1 = tf.Variable(tf.truncated_normal([n_neurons_in_h1],mean=0, stddev=1 / np.sqrt(n_features)), name='biases1')
-y1 = tf.nn.tanh((tf.matmul(X, W1)+b1), name='activationLayer1')
+y1 = tf.nn.sigmoid((tf.matmul(X, W1)+b1), name='activationLayer1')
 
 #network parameters(weights and biases) are set and initialized(Layer2)
 W2 = tf.Variable(tf.random_normal([n_neurons_in_h1, n_neurons_in_h2],mean=0,stddev=1/np.sqrt(n_features)),name='weights2')
@@ -51,33 +49,32 @@ a = tf.nn.sigmoid((tf.matmul(y2, Wo) + bo), name='activationOutputLayer')
 
 #cost function
 #cross_entropy = tf.reduce_mean(-tf.reduce_sum(Y * tf.log(a),reduction_indices=[1]))
-l = tf.losses.mean_squared_error(Y, a)
+error = tf.square(Y - a)
 #optimizer
-train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(l)
+train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(error)
 
 #compare predicted value from network with the expected value/target
-correct_prediction = tf.equal(tf.arg_max(a, 1), tf.arg_max(Y, 1))
+correct_prediction = tf.equal(tf.argmax(a, 1), tf.argmax(Y, 1))
 #accuracy determination
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32), name="Accuracy")
 
 # initialization of all variables
 initial = tf.global_variables_initializer()
 
+#training_data = np.array(list(zip(tr_features, tr_labels)))
 # creating a session
 with tf.Session() as sess:
     sess.run(initial)
     # training loop over the number of epoches
-    batchsize=50
+    mini_batch_size=50
     for epoch in range(training_epochs):
-        for i in range(len(tr_features)):
-            start = i
-            end = i + batchsize
-            x_batch = tr_features[start:end]
-            y_batch = tr_labels[start:end]
+        np.random.shuffle(training_data)
+        mini_batches = [training_data[k:k+mini_batch_size] for k in range(0, len(training_data), mini_batch_size)]
+        for mini_batch in mini_batches:
+            tr_features = np.array(list(map(lambda x: x.flatten(), mini_batch[:, 0])))
+            tr_labels = np.array(list(map(lambda x: x.flatten(), mini_batch[:, 1])))
             # feeding training data/examples
-            sess.run(train_step, feed_dict={X: x_batch, Y: y_batch, keep_prob: 0.5})
-
-            i += batchsize
+            sess.run(train_step, feed_dict={X: tr_features, Y: tr_labels, keep_prob: 0.5})
         # feeding testing data to determine model accuracy
         y_pred = sess.run(a, feed_dict={X: ts_features, keep_prob: 1.0})
         y_true = sess.run(tf.constant(ts_labels))
@@ -85,5 +82,3 @@ with tf.Session() as sess:
         # write results to summary file
         # print accuracy for each epoch
         print('epoch', epoch, acc)
-        print('---------------')
-        print(y_pred, y_true)
