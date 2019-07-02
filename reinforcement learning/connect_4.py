f4f6e0576ecddc457eb8bc9ccdc7c65cdfca9a3c
nmo13 <martin.ennemoser@gmx.at>
2019-07-02 08:19:29 +0200

    Changed to deep net

--- reinforcement learning/connect_4.py
+++ reinforcement learning/connect_4.py
@@ -1,8 +1,137 @@
+from lib import wrappers
+from lib import dqn_model
+
+import argparse
+import time
 import numpy as np
-import random
 import collections
 
-class Game:
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from tensorboardX import SummaryWriter
+
+import gym
+
+MEAN_REWARD_BOUND = 19.5
+
+GAMMA = 0.99
+BATCH_SIZE = 32
+REPLAY_SIZE = 10000
+LEARNING_RATE = 1e-4
+SYNC_TARGET_FRAMES = 100
+REPLAY_START_SIZE = 500
+
+EPSILON_DECAY_LAST_FRAME = 10**5
+EPSILON_START = 1.0
+EPSILON_FINAL = 0.02
+
+HIDDEN_SIZE = 128
+
+ENV = 'connect4'
+class Net(nn.Module):
+    def __init__(self, obs_size, hidden_size, n_actions):
+        super(Net, self).__init__()
+        self.net = nn.Sequential(
+            nn.Linear(obs_size[0], hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, n_actions)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+Experience = collections.namedtuple('Experience', field_names=['state', 'action', 'reward', 'done', 'new_state'])
+
+
+class ExperienceBuffer:
+    def __init__(self, capacity):
+        self.buffer = collections.deque(maxlen=capacity)
+
+    def __len__(self):
+        return len(self.buffer)
+
+    def append(self, experience):
+        self.buffer.append(experience)
+
+    def sample(self, batch_size):
+        indices = np.random.choice(len(self.buffer), batch_size, replace=False)
+        states, actions, rewards, dones, next_states = zip(*[self.buffer[idx] for idx in indices])
+        return np.array(states), np.array(actions), np.array(rewards, dtype=np.double), \
+               np.array(dones, dtype=np.uint8), np.array(next_states)
+
+
+class Agent:
+    def __init__(self, env, exp_buffer, sym, net):
+        self.env = env
+        self.exp_buffer = exp_buffer
+        self.sym = sym
+        self.net = net
+        self._reset()
+
+    def _reset(self):
+        self.state = self.env.reset()
+        self.total_reward = 0.0
+
+    def play_step(self, other_agent, epsilon=0.0, device="cpu"):
+        done_reward = None
+
+        valid_actions = self.env.get_valid_actions()
+        if np.random.random() < epsilon:
+            action = self.env.action_space.sample()
+            while not action in valid_actions:
+                action = self.env.action_space.sample()
+
+        else:
+            state_a = np.array([self.state], copy=False)
+            state_v = torch.tensor(state_a).to(device)
+            #res = self.net.double()
+            #q_vals_v2 = res(state_v.view(9))
+            valid_actions_v = torch.tensor(valid_actions).to(device)
+            q_vals_v = self.net(state_v.view(9).float())
+            valid_q_vals_v = q_vals_v.gather(0, valid_actions_v)
+            x, act_v = torch.max(valid_q_vals_v, dim=0)
+            index = (q_vals_v == x).nonzero()
+            action = int(index)
+
+        # do step in the environment
+        new_state, reward, is_done = self.env.step(action)
+        self.total_reward += reward
+
+        exp = Experience(self.state, action, reward, is_done, new_state)
+        self.exp_buffer.append(exp)
+
+        exp_other = Experience(self.state, action, 0, is_done, new_state)
+        other_agent.exp_buffer.append(exp_other)
+
+        self.state = new_state
+
+        other_agent.state = new_state
+
+        if is_done:
+            done_reward = self.total_reward
+            self._reset()
+            other_agent._reset()
+        return done_reward, is_done
+
+class HumanPlayer(Agent):
+    def __init__(self, env, sym):
+        super(HumanPlayer, self).__init__(env, ExperienceBuffer(REPLAY_SIZE), sym, None)
+
+    epsilon = 0
+    def play_step(self, other_agent, epsilon=0.0, device="cpu"):
+        valid_actions = env.get_valid_actions()
+        while True:
+            # break if we make a legal move
+            move = input("Enter column: ")
+            if int(move) in valid_actions:
+                env.set_sym(int(move), self.sym)
+                break
+        game_over, reward = env.game_over(self.sym)
+        return reward, game_over
+
+class Game(gym.Env):
     ROWS = 3
     COLUMNS = 3
     TILES_TO_WIN = 3
@@ -13,6 +142,8 @@ class Game:
     def __init__(self):
         self.board = np.zeros( (Game.ROWS, Game.COLUMNS) )
         self.actions = np.arange(0, Game.COLUMNS)
+        self.action_space = gym.spaces.Discrete(n=Game.COLUMNS)
+        self.observation_space = gym.spaces.Box(low=-np.inf, high=np.inf, shape=(Game.ROWS * Game.COLUMNS, ), dtype=np.double)
 
     def draw_board(self):
         for i in range(Game.ROWS):
@@ -28,10 +159,9 @@ class Game:
             print("")
         print('----' * Game.COLUMNS)
 
-    def reset(self, p1, p2):
-        self.board = np.zeros((Game.ROWS, Game.COLUMNS))
-        p1.states_actions_rewards = []
-        p2.states_actions_rewards = []
+    def reset(self):
+        self.board = np.zeros((Game.ROWS, Game.COLUMNS), dtype=np.double)
+        return np.copy(self.board)
 
 
     def _other_player(self, cur_player, p1, p2):
@@ -39,7 +169,7 @@ class Game:
             return p2
         return p1
 
-    def get_cur_player(self, p1, p2):
+    def get_cur_player(self):
         r_counter = 0
         y_counter = 0
         for i in range(Game.ROWS):
@@ -54,43 +184,13 @@ class Game:
         elif r_counter < y_counter:
             return p1
 
-        arr = [p1, p2]
-        return arr[np.random.choice(len(arr))]
+        return p1
 
-    def play(self, p1, p2):
-        self.reset(p1, p2)
-        game_over = False
-
-        start_states = list(p1.Q)
-        prev_state = 0#start_states[np.random.choice(len(start_states))] if len(start_states) > 0 else 0
-        #self.set_board(prev_state)
-        cur_player = p1#self.get_cur_player(p1, p2)
-        while not game_over:
-            reward, action, game_over, valid_move = cur_player.move(self)
-            if valid_move:
-                cur_player.states_actions_rewards.append((prev_state, action, reward))
-                self._other_player(cur_player, p1, p2).states_actions_rewards.append((prev_state, action, -reward))
-            else:
-                cur_player.states_actions_rewards.append((prev_state, action, reward))
-                self._other_player(cur_player, p1, p2).states_actions_rewards.append((prev_state, action, reward))
-            cur_player = p2 if cur_player == p1 else p1
-            prev_state = self.get_state()
-            if debug:
-                self.draw_board()
-                print(self.get_state())
-
-
-        return self.create_sar(p1.states_actions_rewards), self.create_sar(p2.states_actions_rewards)
-
-    def create_sar(self, states_actions_rewards):
-        sar = []
-        G = 0
-        for s, a, r in reversed(states_actions_rewards):
-            G = r + Game.GAMMA * G
-            sar.append((s, a, G))
-
-        sar.reverse()
-        return sar
+    def step(self, action):
+        player = self.get_cur_player()
+        env.set_sym(action, player.sym)
+        game_over, reward = env.game_over(player.sym)
+        return np.copy(env.board), reward, game_over
 
     def set_sym(self, col, sym):
         col_vals = self.board[:,col]
@@ -112,32 +212,34 @@ class Game:
         return False
 
     def game_over(self, sym):
-        if not 0 in self.board:
-            return True, 0
+        reward = 10
 
         row = self.board[self.latest_move[0]]
         if self._check_adjacency(row, sym):
-            return True, 1
+            return True, reward
         if self._check_adjacency(row, -sym):
-            return True, -1
+            return True, 0
 
         col = self.board[:,self.latest_move[1]]
         if self._check_adjacency(col, sym):
-            return True, 1
+            return True, reward
         if self._check_adjacency(col, -sym):
-            return True, -1
+            return True, 0
 
         diag = self.get_diagonal(self.latest_move)
         if self._check_adjacency(diag, sym):
-            return True, 1
+            return True, reward
         if self._check_adjacency(diag, -sym):
-            return True, -1
+            return True, 0
 
         diag = self.get_diagonal(self.latest_move, True)
         if self._check_adjacency(diag, sym):
-            return True, 1
+            return True, reward
         if self._check_adjacency(diag, -sym):
-            return True, -1
+            return True, 0
+
+        if not 0 in self.board:
+            return True, 0
 
         return False, 0
 
@@ -225,59 +327,22 @@ class Game:
                 valid_actions.append(i)
         return valid_actions
 
-class Player:
-    def __init__(self, sym):
-        self.sym = sym
-        self.Q = {}
-        self.states_actions_rewards = []
-        self.policy = {}
-        self.return_dict = {}
+def calc_loss(batch, net, tgt_net, device="cpu"):
+    states, actions, rewards, dones, next_states = batch
 
-    def move(self, env):
-        number = np.random.uniform()
-        action = None
-        if number <= epsilon:
-            # explore
-            valid_actions = env.get_valid_actions()
-            action = random.choice(valid_actions)
-        else:
-            # exploit
-            action = self.policy.get(env.get_state(), None)
-            if action == None:
-                if debug:
-                    print('Unknown state')
-                valid_actions = env.get_valid_actions()
-                action = random.choice(valid_actions)
+    states_v = torch.tensor(states).float().to(device)
+    next_states_v = torch.tensor(next_states).float().to(device)
+    actions_v = torch.tensor(actions).to(device)
+    rewards_v = torch.tensor(rewards).float().to(device)
+    done_mask = torch.ByteTensor(dones).to(device)
 
-        if env.board[0, action]:
-            return -100, action, True, False
-        env.set_sym(action, self.sym)
-        game_over, reward = env.game_over(self.sym)
-        return reward, action, game_over, True
+    state_action_values = net(states_v.view((-1, 9))).gather(1, actions_v.unsqueeze(-1)).squeeze(-1)
+    next_state_values = tgt_net(next_states_v.view((-1, 9))).max(1)[0]
+    next_state_values[done_mask] = 0.0
+    next_state_values = next_state_values.detach()
 
-    def update_q_and_policy(self, sar):
-        for state, action, G in sar:
-            sa = (state, action)
-            self.return_dict.setdefault(sa, []).append(G)
-            aval = self.Q.setdefault(state, {})
-            aval.setdefault(action, 0)
-            aval[action] = np.mean(self.return_dict[sa])
-            action = max(self.Q[state].keys(), key=(lambda key: self.Q[state][key]))
-            self.policy[state] = action
-
-class HumanPlayer(Player):
-    epsilon = 0
-    def move(self, env):
-        valid_actions = env.get_valid_actions()
-        while True:
-            # break if we make a legal move
-            move = input("Enter column: ")
-            if int(move) in valid_actions:
-                env.set_sym(int(move), self.sym)
-                break
-        game_over, reward = env.game_over(self.sym)
-        return reward, 0, game_over, True
-
+    expected_state_action_values = next_state_values * GAMMA + rewards_v
+    return nn.MSELoss()(state_action_values, expected_state_action_values)
 
 if __name__ == "__main__":
     # Exploration parameters
@@ -288,36 +353,114 @@ if __name__ == "__main__":
     debug = False
     import time
     start = time.time()
-    mw = Game()
+    env = Game()
 
-    p1 = Player(Game.RED)
-    p2 = Player(Game.YELLOW)
+    device = torch.device("cuda")
 
-    for episode in range(10000):
-        epsilon = min_epsilon + (max_epsilon - min_epsilon) * np.exp(-episode / decay_rate)
-        if episode % 200 == 0:
-            print('Episode:', str(episode))
-            print('Epsilon: ' + str(epsilon))
-            print('Policy count for iteration {0}: {1}'.format(episode, len(p1.policy)))
-        sar1, sar2 = mw.play(p1, p2)
-        p1.update_q_and_policy(sar1)
-        p2.update_q_and_policy(sar2)
+    net1 = Net(env.observation_space.shape, HIDDEN_SIZE, env.action_space.n).to(device)
+    tgt_net1 = Net(env.observation_space.shape, HIDDEN_SIZE, env.action_space.n).to(device)
+    optimizer1 = optim.Adam(net1.parameters(), lr=LEARNING_RATE)
 
-        # Reduce epsilon (because we need less and less exploration)
-        #epsilon = 1/(episode+1)#min_epsilon + (max_epsilon - min_epsilon) * np.exp(-decay_rate * episode)
-    end = time.time()
-    print('Finished')
-    print('time: {}'.format(end - start))
+    net2 = Net(env.observation_space.shape, HIDDEN_SIZE, env.action_space.n).to(device)
+    tgt_net2 = Net(env.observation_space.shape, HIDDEN_SIZE, env.action_space.n).to(device)
+    optimizer2 = optim.Adam(net2.parameters(), lr=LEARNING_RATE)
 
-    p1 = HumanPlayer(Game.RED)
+
+    writer = SummaryWriter(comment="-" + ENV)
+
+    buffer1 = ExperienceBuffer(REPLAY_SIZE)
+    buffer2 = ExperienceBuffer(REPLAY_SIZE)
+    p1 = Agent(env, buffer1, Game.RED, net1)
+    p2 = Agent(env, buffer2, Game.YELLOW, net2)
+    epsilon = EPSILON_START
+
+    total_rewards = []
+    frame_idx = 0
+    ts_frame = 0
+    ts = time.time()
+    best_mean_reward = None
+    current_player = p1
+
+    while True:
+        frame_idx += 1
+        epsilon = max(EPSILON_FINAL, EPSILON_START - frame_idx / EPSILON_DECAY_LAST_FRAME)
+
+        if current_player is p1:
+            done, reward = p1.play_step(p2, epsilon, device=device)
+            current_player = p2
+        else:
+            done, reward = p2.play_step(p1, epsilon, device=device)
+            current_player = p1
+
+        if done and current_player is p2:
+            total_rewards.append(reward)
+            speed = (frame_idx - ts_frame) / (time.time() - ts)
+            ts_frame = frame_idx
+            ts = time.time()
+            mean_reward = np.mean(total_rewards[-100:])
+            print("%d: done %d games, mean reward %.3f, eps %.2f, speed %.2f f/s" % (
+                frame_idx, len(total_rewards), mean_reward, epsilon,
+                speed
+            ))
+            print("Buffer size: %d" % len(buffer1))
+            writer.add_scalar("epsilon", epsilon, frame_idx)
+            writer.add_scalar("speed", speed, frame_idx)
+            writer.add_scalar("reward_100", mean_reward, frame_idx)
+            writer.add_scalar("reward", reward, frame_idx)
+            if best_mean_reward is None or best_mean_reward < mean_reward:
+                torch.save(net1.state_dict(), ENV + "-best.dat")
+                if best_mean_reward is not None:
+                    print("Best mean reward updated %.3f -> %.3f, model saved" % (best_mean_reward, mean_reward))
+                best_mean_reward = mean_reward
+            current_player = p1
+            if len(total_rewards) > 1000:
+                print("Solved in %d frames!" % frame_idx)
+                break
+
+        assert(len(buffer1) == len(buffer2))
+        if len(buffer1) < REPLAY_START_SIZE:
+            continue
+
+        if frame_idx % SYNC_TARGET_FRAMES == 0:
+            tgt_net1.load_state_dict(net1.state_dict())
+            tgt_net2.load_state_dict(net2.state_dict())
+
+        optimizer1.zero_grad()
+        loss_t1 = calc_loss(buffer1.sample(BATCH_SIZE), net1, tgt_net1, device=device)
+        loss_t1.backward()
+        optimizer1.step()
+        writer.add_scalar("loss1", loss_t1.item(), frame_idx)
+
+        optimizer2.zero_grad()
+        loss_t2 = calc_loss(buffer2.sample(BATCH_SIZE), net2, tgt_net2, device=device)
+        loss_t2.backward()
+        optimizer2.step()
+
+        writer.add_scalar("loss2", loss_t2, frame_idx)
+
+    writer.close()
+
+    env = Game()
+    p1.env = env
+    p2 = HumanPlayer(env, Game.YELLOW)
     epsilon = 0
 
+    current_player = p1
     debug = True
     while True:
-        mw.play(p1, p2)
+        done = False
+        while not done:
+            if current_player is p1:
+                reward, done = p1.play_step(p2, 0, device=device)
+                current_player = p2
+                env.draw_board()
+            else:
+                reward, done = p2.play_step(p1, 0, device=device)
+                current_player = p1
+
+        current_player = p1
+        env.reset()
+
         answer = input("Play again? [Y/n]: ")
         if answer and answer.lower()[0] == 'n':
             break
-
-
-
